ed0fe39883f2aa2d3fc7cf7532b2ec6e7ad037f5
andrewweston <andrew.weston@propellerhead.co.nz>
2022-02-24 11:53:23 +1300

    tag:patch] bump

--- packages/core/src/Request/useRequest.jsx
+++ packages/core/src/Request/useRequest.jsx
@@ -98,7 +98,6 @@ export const useRequest = (endpoint) => {
         }
     }, [endpoint, checkStatus]);
 
-    
     return useMemo(() => {
         return { getQRCode, qrCode, status, payload };
     }, [getQRCode, qrCode, status, payload]);
