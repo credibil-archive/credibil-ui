d2d45629da3b080a1743bdccf2bdb5cea0c87c85
andrewweston <andrew.weston@propellerhead.co.nz>
2022-03-04 15:02:56 +1300

    switch to workspaces instead of npm link

--- debug/auth/npm_link.sh
+++ debug/auth/npm_link.sh
@@ -4,7 +4,9 @@
 APP_DIR=$PWD
 PKG_DIR="$PWD/../../packages/auth"
 
-# link @credibil/core package source
-npm link $PKG_DIR
+# link @credibil/auth package source
+# !!don't need this when using workspaces!!
+# npm link $PKG_DIR
+
 # link @credibil/auth's version of react to this package  
 cd $PKG_DIR && npm link $APP_DIR/node_modules/react && cd $APP_DIR
--- debug/core/npm_link.sh
+++ debug/core/npm_link.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# !!don't need this script when using workspaces!!
+
 # link @credibil/core library source 
 APP_DIR=$PWD
 PKG_DIR="$PWD/../../packages/core"
@@ -8,12 +10,5 @@ PKG_DIR="$PWD/../../packages/core"
 npm link $PKG_DIR
 # link @credibil/core's version of react to this package  
 cd $PKG_DIR && npm link $APP_DIR/node_modules/react && cd $APP_DIR
-
-# Using Yarn link instead
-# cd $PKG_DIR/core && yarn link
-# cd $APP_DIR && yarn link @credibil/core
-
-# cd $APP_DIR/node_modules/react && yarn link
-# cd $PKG_DIR/core && yarn link "react"
 
 # https://benjaminwfox.com/blog/tech/why-isnt-npm-link-working
